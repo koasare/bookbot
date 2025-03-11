548a576ae130d849367485014ff4e878373142ba
koasare <koasare@live.com>
2025-03-10 22:28:54 -0500

    updated bookbot to include sys argument

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import stats
 
 def get_book_text(filepath):
@@ -11,14 +12,19 @@ def get_book_text(filepath):
         return f"An error occurred: {e}"
     
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    
     """Reads and prints the contents of frankenstein.txt."""
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text(book_path)
     num_words = stats.count_words(book_text)
     num_chars = stats.count_characters(book_text)
     sorted_counts = stats.sort_char_counts(num_chars)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
